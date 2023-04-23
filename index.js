fcfd87eef76796224b70a48db51399bd85ff6879
Victor <victor.ireri@student.moringaschool.com>
2023-04-23 11:22:44 +0300

    labs

---
+++ index.js
@@ -0,0 +1,44 @@
+
+import React, { useState } from 'react';
+
+function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(`Submitting form with name=${name}, email=${email}, message=${message}`);
+  }
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+
+  function handleMessageChange(event) {
+    setMessage(event.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={handleNameChange} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={handleEmailChange} />
+      </label>
+      <label>
+        Message:
+        <textarea value={message} onChange={handleMessageChange} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
